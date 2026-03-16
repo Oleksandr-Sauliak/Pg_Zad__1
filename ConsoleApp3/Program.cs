468366de85f7a8fc8e3b27a965706b88e388655c
146283 <146283@student.uni.opole.pl>
2026-03-16 10:11:35 +0100

    3 Commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Worldddddddddddddddddddddddddddddd!");
+Console.WriteLine("3 Commit!");
